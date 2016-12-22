d2983676e6985701fd6695e4f5095d34c8e9b5fb
耿小泰 <clawpo@foxmail.com>
2016-12-22 15:51:52 +0800

    初步尝试使用androidannotations注入

--- app/src/main/java/cn/ucai/androidannotationsdemo/MainActivity.java
+++ app/src/main/java/cn/ucai/androidannotationsdemo/MainActivity.java
@@ -1,13 +1,15 @@
 package cn.ucai.androidannotationsdemo;
 
 import android.app.Activity;
-import android.os.Bundle;
 
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
 }
